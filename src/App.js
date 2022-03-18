81a733550949824acf0a3dc9d0cc22ef1e84acf4
gilkag2 <gilkag2@gmail.com>
2022-03-18 14:38:39 +0200

    created the app layout, and created the budget card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import Container from 'react-bootstrap/Container';
+import { Stack, Button } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
 function App() {
   return (
-    <div>
-
-    </div>
-  )
+    <Container className='my-4'>
+      <Stack direction='horizontal' gap="2" className="mb-4">
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='outline-primary'>Add Expense</Button>
+      </Stack>
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+        gap: "1rem",
+        alignItems: "flex-start "
+      }}></div>
+      <BudgetCard name="Entertainment" gray amount={200} max={1000}></BudgetCard>
+    </Container>
+  );
 }
 
 export default App;
